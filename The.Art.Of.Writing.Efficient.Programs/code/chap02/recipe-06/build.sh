d2d49c1caafa832080c45ef88ef91480081ec2ef
hexu <hexu_1985@sina.com>
2024-06-13 16:37:06 +0800

    update The.Art.Of.Writing.Efficient.Programs/code/chap02/recipe-06

--- The.Art.Of.Writing.Efficient.Programs/code/chap02/recipe-06/build.sh
+++ The.Art.Of.Writing.Efficient.Programs/code/chap02/recipe-06/build.sh
@@ -5,4 +5,5 @@ clang++ -g -O3 -mavx2 -Wall -pedantic compare.C example.C -o example
 sudo bash -c "echo -1 > /proc/sys/kernel/perf_event_paranoid"
 perf record ./example
 
-perf report
+#perf report
+perf report --stdio
