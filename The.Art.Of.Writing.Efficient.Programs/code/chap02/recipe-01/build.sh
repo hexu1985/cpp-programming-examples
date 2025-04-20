8b46758d3c470388489ae35f4aab6183a4f36a24
hexu <hexu_1985@sina.com>
2025-04-20 14:49:10 +0800

    update The.Art.Of.Writing.Efficient.Programs/code/chap02/recipe-01

--- The.Art.Of.Writing.Efficient.Programs/code/chap02/recipe-01/build.sh
+++ The.Art.Of.Writing.Efficient.Programs/code/chap02/recipe-01/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -x
-clang++ -g -O3 -mavx2 -Wall -pedantic compare.C example.C -o example && ./example
+clang++ -g -O3 -mavx2 -Wall -pedantic 01_substring_sort.C 01_substring_sort_a.C -o example && ./example
