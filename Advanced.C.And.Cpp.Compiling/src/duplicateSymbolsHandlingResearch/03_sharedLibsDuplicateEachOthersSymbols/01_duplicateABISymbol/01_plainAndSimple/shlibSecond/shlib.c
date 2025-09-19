b0eb5707d75180b2bb6542bfc9e17b1de4e02f09
hexu <hexu_1985@sina.com>
2025-09-19 14:43:48 +0800

    update Advanced.C.And.Cpp.Compiling/src/duplicateSymbolsHandlingResearch/03_sharedLibsDuplicateEachOthersSymbols/01_duplicateABISymbol/01_plainAndSimple/shlibSecond/shlib.c

--- Advanced.C.And.Cpp.Compiling/src/duplicateSymbolsHandlingResearch/03_sharedLibsDuplicateEachOthersSymbols/01_duplicateABISymbol/01_plainAndSimple/shlibSecond/shlib.c
+++ Advanced.C.And.Cpp.Compiling/src/duplicateSymbolsHandlingResearch/03_sharedLibsDuplicateEachOthersSymbols/01_duplicateABISymbol/01_plainAndSimple/shlibSecond/shlib.c
@@ -15,6 +15,6 @@ int shlibsecond_function(void)
 int shlibsecond_another_function(void)
 {
 	printf("%s\n", __FUNCTION__);
-	shlib_function();
+	shlib_function();       // internal call to the duplicate ABI function
 	return 0;
 }
