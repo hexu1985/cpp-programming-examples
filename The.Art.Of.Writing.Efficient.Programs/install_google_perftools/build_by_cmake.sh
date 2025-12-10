b34598703f4bd6649224c0aaa5944aa2f8ae64ea
hexu <hexu_1985@sina.com>
2025-12-10 11:26:42 +0800

    update The.Art.Of.Writing.Efficient.Programs/install_google_perftools

--- The.Art.Of.Writing.Efficient.Programs/install_google_perftools/build_by_cmake.sh
+++ The.Art.Of.Writing.Efficient.Programs/install_google_perftools/build_by_cmake.sh
@@ -1,3 +1,11 @@
-cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/local/gperftools
-cmake --build build --target install --parallel 4 
+#!/usr/bin/bash
 
+current_file_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
+cd ${current_file_dir}
+project_root_dir=$( pwd )
+
+echo ${project_root_dir}
+cmake -S. -Bbuild -Dgperftools_enable_broken_install_targets=ON
+cmake --build build
+
+cmake --install build --prefix ~/local/gperftools
